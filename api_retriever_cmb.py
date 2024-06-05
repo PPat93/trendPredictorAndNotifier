51f60342307571ce1daa07c74404bdad4f1cbd22
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-05 23:03:17 +0100

    v0.02.0 - reshape retriever to be a function, remove unnecessary call with ibm ticker

--- api_retriever_cmb.py
+++ api_retriever_cmb.py
@@ -11,8 +11,11 @@ import requests
 
 API_KEY = os.environ.get("predictorRetrieverApiKey")
 
-API_URI = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={API_KEY}&outputsize=compact"
-
-res = requests.get(API_URI, timeout=10)
-
-print(f"Status: {res.status_code}, Res: {res.content}")
+# TODO - to be expanded and refactoredto be able to retrieve different metrics and symbols with various settings
+def retrieve_data(stock_ticker):
+    """Retrieve stock data example"""
+    api_uri= f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock_ticker}&interval=5min&apikey={API_KEY}&outputsize=compact"
+    res = requests.get(api_uri, timeout=10)
+    retrieved_data = res.content
+    print(f"Status: {res.status_code}, Res: {retrieved_data}")
+    return res
