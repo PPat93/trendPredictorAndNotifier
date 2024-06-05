1c4badbfb14770b4a741ee8b3ae11ba25ee1f8ba
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-05 21:53:17 +0100

    v0.01.0 - last updates on retriever spike

--- api_retriever_cmb.py
+++ api_retriever_cmb.py
@@ -9,10 +9,10 @@ Extract values from the response and deletes everything that is not needed;
 import os
 import requests
 
-API_KEY = os.environ.get("OPENAI_API_KEY")
+API_KEY = os.environ.get("predictorRetrieverApiKey")
 
 API_URI = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={API_KEY}&outputsize=compact"
 
-r = requests.get(API_URI, timeout=10)
+res = requests.get(API_URI, timeout=10)
 
-print(f"Status: {r.status_code}, Res: {r.content}")
+print(f"Status: {res.status_code}, Res: {res.content}")
