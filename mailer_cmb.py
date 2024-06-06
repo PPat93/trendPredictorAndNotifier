f5e804428c6b62114e0f176c4b3bafa0d4751c39
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-07 00:31:07 +0100

    v0.03.0 - add proper sections to the email - title, to, from, content. Add usage of the EmailMessage instead of raw email creation

--- mailer_cmb.py
+++ mailer_cmb.py
@@ -8,18 +8,30 @@ Sends email;
 import smtplib
 import ssl
 import os
+from email.message import EmailMessage
+
 from api_retriever_cmb import retrieve_data
 
 
 PORT = 465
+TICKER = "CMA"
 sender_email = os.environ.get("predictorMailerAddress")
 sender_pass = os.environ.get("predictorAppPass")
 notifyee_email = os.environ.get("notifyeeMailerAddress")
-retriever_res = retrieve_data("IBM").content
-message = "Hi! \n" #+ str(retriever_res)
+
+retriever_res = retrieve_data(TICKER).content
+
+compose_message = EmailMessage()
+
+compose_message["Subject"] = f"Change on {TICKER}"
+compose_message["From"] = sender_email
+compose_message["To"] = notifyee_email
+compose_message.set_content(
+    f"Hi! \nI found an interesting change on {TICKER}.\n{retriever_res}"
+)
 
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", PORT, context=context) as server:
     server.login(sender_email, sender_pass)
-    server.sendmail(sender_email, notifyee_email, message)
+    server.send_message(compose_message)
