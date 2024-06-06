abb5f65e791be9ae802fac1d9ba70dd33d3f1105
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-06 23:23:46 +0100

    v0.03.0 - prepare working spike of the automatic email sender - first ver.

--- mailer_cmb.py
+++ mailer_cmb.py
@@ -4,3 +4,22 @@ Receives data from Integrator: -> Analyzer, -> Database;
 Creates email and it's content;
 Sends email;
 """
+
+import smtplib
+import ssl
+import os
+from api_retriever_cmb import retrieve_data
+
+
+PORT = 465
+sender_email = os.environ.get("predictorMailerAddress")
+sender_pass = os.environ.get("predictorAppPass")
+notifyee_email = os.environ.get("notifyeeMailerAddress")
+retriever_res = retrieve_data("IBM").content
+message = "Hi! \n" #+ str(retriever_res)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", PORT, context=context) as server:
+    server.login(sender_email, sender_pass)
+    server.sendmail(sender_email, notifyee_email, message)
