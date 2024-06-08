8ce0b69b3ebd8427a976f2b945537b2f5d61fee8
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-08 18:50:21 +0100

    v0.04.0 - api retriever refactor - clean prints, add comments and unify returned values + add crypto price retrieving method

--- api_retriever_cmb.py
+++ api_retriever_cmb.py
@@ -13,14 +13,37 @@ import requests
 
 FUNDAMENTAL_API_KEY = os.environ.get("fundamentalRetrieverApiKey")
 PRICE_API_KEY = os.environ.get("realtimeRetrieverApiKey")
+HEADERS = {"Content-Type": "application/json"}
 
-def retrieve_last_price(stock_ticker):
-    """Retrieve stock data example"""
+
+# shared with crypto price - max 50 reqs/hour, 1000 reqs/day
+def retrieve_last_stock_price(stock_ticker):
+    """Retrieve latest stock price"""
     price_api_uri = f"https://api.tiingo.com/iex/{stock_ticker}?token={PRICE_API_KEY}"
-    headers = {"Content-Type": "application/json"}
 
-    price_res = requests.get(price_api_uri, headers=headers, timeout=10)
-    latest_price_json = float((price_res.json())[0].get("last"))
+    price_res = requests.get(price_api_uri, headers=HEADERS, timeout=10)
+    price_json = price_res.json()
 
-    print(f" yo {latest_price_json}")
-    return latest_price_json
+    return price_json
+
+
+# max 25 reqs/day
+def retrieve_stock_fundamental_data(stock_ticker):
+    """Retrieve fundamental data for the specified stock"""
+    fundamental_api_uri = f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={stock_ticker}&apikey={FUNDAMENTAL_API_KEY}"
+
+    fundamental_res = requests.get(fundamental_api_uri, headers=HEADERS, timeout=10)
+    fundamental_json = fundamental_res.json()
+
+    return fundamental_json
+
+
+# shared with stock price - max 50 reqs/hour, 1000 reqs/day
+def retrieve_last_crypto_price(crypto_ticker):
+    """Retrieve latest crypto price"""
+    crypto_api_uri = f"https://api.tiingo.com/tiingo/crypto/top?tickers={crypto_ticker}&token={PRICE_API_KEY}"
+
+    crypto_res = requests.get(crypto_api_uri, headers=HEADERS, timeout=10)
+    crypto_json = crypto_res.json()
+
+    return crypto_json
