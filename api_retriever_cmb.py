61570599dea34421ec00133e56e306d5398729e2
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-08 18:53:30 +0100

    v0.04.0 - remove typo

--- api_retriever_cmb.py
+++ api_retriever_cmb.py
@@ -3,7 +3,7 @@ API data retriever comb:
 Sends data to Integrator -> Analyzer;
 Sends API request to retrieve needed values;
 Receives response with retrieved values;
-Extract values from the response and deletes everything that is not needed;1
+Extract values from the response and deletes everything that is not needed;
 """
 
 # TODO - to be expanded and refactoredto be able to retrieve different metrics and symbols with various settings
