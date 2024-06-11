98073efd0a442deceeb6fc2e0789796eec1bf424
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-11 23:21:55 +0100

    v0.06.0 - prepare aalyzer, cleanup and addition of the new folder for calculation files + create and finish RSI calc hex

--- analyzer_cmb.py
+++ analyzer_cmb.py
@@ -7,55 +7,15 @@ Remove old reference data if expected;
 """
 
 from api_retriever_cmb import retrieve_last_stock_price
-import talib
-import yfinance as yf
-import matplotlib.pyplot as plt
+from  trendPredictor.analyzer_src.rsi_calc_hex import calc_rsi
 
-
-def run_trend_analysis(ticker: str):
+def run_trend_analysis(ticker: str, start_date: str, end_date: str):
 
     latest_price = float((retrieve_last_stock_price(ticker))[0].get("last"))
+    
+    calculated_rsi = calc_rsi(ticker, start_date, end_date)
 
     trend_is_reversed = (
         False  # TODO bool returned with a value if the trend was reversed
     )
     return trend_is_reversed
-
-
-microsoft = yf.Ticker("NVDA")
-dict = microsoft.info
-dataRes = yf.download("NVDA", start="2024-06-04", end="2024-06-07", interval="5m")
-
-
-def RSI(data, window=14, adjust=False):
-    delta = data["Close"].diff(1).dropna()
-    loss = delta.copy()
-    gains = delta.copy()
-
-    gains[gains < 0] = 0
-    loss[loss > 0] = 0
-
-    gain_ewm = gains.ewm(com=window - 1, adjust=adjust).mean()
-    loss_ewm = abs(loss.ewm(com=window - 1, adjust=adjust).mean())
-
-    RS = gain_ewm / loss_ewm
-    RSI = 100 - 100 / (1 + RS)
-    return RSI
-
-
-# RSI(dataRes)
-reversed_df = dataRes.iloc[::-1]
-
-# pylint: disable=no-member
-dataRes["RSI"] = talib.RSI(reversed_df["Close"], 14)
-
-ax1 = plt.subplot2grid((10, 1), (0, 0), rowspan=4, colspan=1)
-ax2 = plt.subplot2grid((10, 1), (5, 0), rowspan=4, colspan=1)
-ax1.plot(dataRes["Close"], linewidth=2.5)
-ax1.set_title("Nvidia usd")
-ax2.plot(dataRes["RSI"], color="red", linewidth=1.5)
-ax2.axhline(30, linestyle="--", linewidth=1.5, color="grey")
-ax2.axhline(70, linestyle="--", linewidth=1.5, color="grey")
-ax2.set_title("Nvidia RSI")
-
-plt.show()
---
+++ analyzer_src/rsi_calc_hex.py
@@ -0,0 +1,39 @@
+"""
+RSI hex:
+Calculates The Relative Strength Index for a given stock ticker.
+The given time intervals will be used to determine the range of calculations.
+Window parameter defines how much the old data influences the new data. 
+    - Scientificly: Specifies decay in terms of center of mass. 
+    - The larger the window, the greater the influence of older data on newer data.
+Base calculations are given in the comment. However, the Ta-Lib library was used 
+for the calculations.
+"""
+
+import talib
+import yfinance as yf
+
+# def calc_rsi(data, window=14, adjust=False):
+#     delta = data["Close"].diff(1).dropna()
+#     loss = delta.copy()
+#     gains = delta.copy()
+
+#     gains[gains < 0] = 0
+#     loss[loss > 0] = 0
+
+#     gain_ewm = gains.ewm(com=window - 1, adjust=adjust).mean()
+#     loss_ewm = abs(loss.ewm(com=window - 1, adjust=adjust).mean())
+
+#     RS = gain_ewm / loss_ewm
+#     RSI = 100 - 100 / (1 + RS)
+#     return RSI
+
+
+def calc_rsi(ticker: str, start_date: str, end_date: str):
+    """Retrieve specified stock data range and calculate its RSI"""
+
+    data_res = yf.download(ticker, start_date, end_date)
+
+    # pylint: disable=no-member
+    data_res["RSI"] = talib.RSI(data_res["Close"], 14)
+
+    return data_res["RSI"]
