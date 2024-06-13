631b729b6124eb5a02360bdc187f2e5551602df7
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-13 23:39:03 +0100

    v0.06.01 - add macd description + rsi desc update

--- analyzer_src/macd_calc_hex.py
+++ analyzer_src/macd_calc_hex.py
@@ -1,12 +1,17 @@
 """
 MACD hex:
 Calculates The Moving Average Convergence/Divergence for a given stock ticker.
-
+Helps with identification of price trends and measurement oftrend momentum. 
+Also, it shows points for buying and selling. However, it needs other factors and 
+ratios cause if used alone it might generate false signals. Great with RSI.
+Base calculations are given in the comment. However, the Ta-Lib library was used 
+for the calculations.
 """
 
 import talib
 import yfinance as yf
 
+# MACD=12-Period EMA − 26-Period EMA
 
 def calc_macd(ticker: str, start_date: str, end_date: str):
     """Retrieve specified stock data range and calculate its MACD"""
