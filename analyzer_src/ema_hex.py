4e364a004180dfe5863d4367e1efc193502f511a
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-17 23:41:54 +0100

    V0.06.2 - preliminary dates preparations for stock retrieval in ema

--- analyzer_src/ema_hex.py
+++ analyzer_src/ema_hex.py
@@ -3,6 +3,7 @@ EMA hex:
 Calculates Exponential Moving Average for a given stock ticker.
 """
 
+from datetime import date, timedelta
 import yfinance as yf
 
 # EMA = Price(today)×k+EMA(yesterday)×(1−k); where: k=2÷(N+1), n = number of days
@@ -12,12 +13,16 @@ import yfinance as yf
 def calc_ema(ticker: str, period: int):
     """Retrieve specified stock and calculate its EMA"""
 
-    # todo attach dates to automatically calculate expected period for retrieval - starting from current day"
-    # something like: startd_date = {today}, end_date = {today - period}
-    res = yf.download(ticker, interval="1d")
+    current_date = str(date.today())
+    start_date = str(date.today() - timedelta(days=period))
     
+    # TODO exclude holidays
+
+    res = yf.download(ticker, start_date, current_date)
+
     res["EMA"] = res["Close"].ewm(span=period, adjust=False).mean()
 
     return res["EMA"]
 
-calc_ema("nvda", 12)
+
+print(calc_ema("nvda", 12))
