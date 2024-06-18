a402ef72c23c2127d568b7b8d98dedb25c3c2e5b
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-18 23:56:49 +0100

    v0.06.2 - preliminary version of data retrieval excluding holidays and other no trade periods

--- analyzer_src/ema_hex.py
+++ analyzer_src/ema_hex.py
@@ -5,6 +5,8 @@ Calculates Exponential Moving Average for a given stock ticker.
 
 from datetime import date, timedelta
 import yfinance as yf
+import math
+import pandas as pd
 
 # EMA = Price(today)×k+EMA(yesterday)×(1−k); where: k=2÷(N+1), n = number of days
 # the above calculation formula assumes 1 day = 1 period
@@ -14,15 +16,37 @@ def calc_ema(ticker: str, period: int):
     """Retrieve specified stock and calculate its EMA"""
 
     current_date = str(date.today())
-    start_date = str(date.today() - timedelta(days=period))
-    
-    # TODO exclude holidays
 
-    res = yf.download(ticker, start_date, current_date)
+    # Exclude weekends and potential holidays
+    # "period" - period itself; "period/7*2" - number of average weekend days per each week
+    # retrieved; "+2" - two more days in case that data is retrieved on Sunday;
+    # "potential_public_holidays" - in case there are multiple holidays/free days/no trading
+    # during longer periods retrieved;
+    if period > 150:
+        potential_public_holidays = 200
+    elif period < 7:
+        potential_public_holidays = 5
+    else:
+        potential_public_holidays = 50
+
+    period_retrieved = (
+        period + math.ceil(period / 7 * 2) + 2 + potential_public_holidays
+    )
+    print("per ret " + str(period_retrieved))
+    start_date = str(date.today() - timedelta(days=period_retrieved))
+    # start_date = str(date.today() - timedelta(days=period))
+    res = yf.download(ticker, start=start_date, end=current_date)
+    print("dates: " + start_date + " end " + current_date)
+    print("pre")
+    print(res)
+    print(period)
+    res2 = res.tail(period)
+    print("post")
+    print(res2)
 
     res["EMA"] = res["Close"].ewm(span=period, adjust=False).mean()
 
     return res["EMA"]
 
 
-print(calc_ema("nvda", 12))
+calc_ema("nvda", 500)
