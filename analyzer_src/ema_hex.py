1d4b140869868fe77891a82fd7946d3a79c887ef
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-19 23:17:54 +0100

    v0.06.2 - add method calculating real days period  - including weekends, adjust holidays, clean ema counting method, add comments

--- analyzer_src/ema_hex.py
+++ analyzer_src/ema_hex.py
@@ -5,8 +5,6 @@ Calculates Exponential Moving Average for a given stock ticker.
 
 from datetime import date, timedelta
 import yfinance as yf
-import math
-import pandas as pd
 
 # EMA = Price(today)×k+EMA(yesterday)×(1−k); where: k=2÷(N+1), n = number of days
 # the above calculation formula assumes 1 day = 1 period
@@ -15,38 +13,45 @@ import pandas as pd
 def calc_ema(ticker: str, period: int):
     """Retrieve specified stock and calculate its EMA"""
 
-    current_date = str(date.today())
+    current_date = date.today()
+    potential_public_holidays = 20
 
-    # Exclude weekends and potential holidays
-    # "period" - period itself; "period/7*2" - number of average weekend days per each week
-    # retrieved; "+2" - two more days in case that data is retrieved on Sunday;
-    # "potential_public_holidays" - in case there are multiple holidays/free days/no trading
+    # Include potential holidays - as there are multiple holidays/free days/no trading
     # during longer periods retrieved;
-    if period > 150:
-        potential_public_holidays = 200
-    elif period < 7:
-        potential_public_holidays = 5
-    else:
-        potential_public_holidays = 50
+    if period < 7:
+        potential_public_holidays = 2
+    elif period > 150:
+        potential_public_holidays = 40
 
-    period_retrieved = (
-        period + math.ceil(period / 7 * 2) + 2 + potential_public_holidays
+    period_including_non_working_days = (
+        include_weekends_in_days_substract(date.today(), period)
+        + potential_public_holidays
     )
-    print("per ret " + str(period_retrieved))
-    start_date = str(date.today() - timedelta(days=period_retrieved))
-    # start_date = str(date.today() - timedelta(days=period))
-    res = yf.download(ticker, start=start_date, end=current_date)
-    print("dates: " + start_date + " end " + current_date)
-    print("pre")
-    print(res)
-    print(period)
-    res2 = res.tail(period)
-    print("post")
-    print(res2)
 
-    res["EMA"] = res["Close"].ewm(span=period, adjust=False).mean()
+    # Calculate expected retrieval date
+    start_date = date.today() - timedelta(days=period_including_non_working_days)
+    data_retrieved = yf.download(ticker, start=start_date, end=current_date)
+    # Adjust retrieved data to have specified periods
+    data_retrieved = data_retrieved.tail(period)
 
-    return res["EMA"]
+    # Calculate Exponential Moving Average
+    ema = data_retrieved["Close"].ewm(span=period, adjust=False).mean()
+
+    return ema
 
 
-calc_ema("nvda", 500)
+def include_weekends_in_days_substract(date, workdays_to_substract: int):
+    """Include weekends in the working days provided in workdays_to_substract to
+    calculate how many days actually needs to be substracted to reach the
+    period of workdays_to_substract workdays"""
+    all_days_to_substract = 0
+    temp_date = date
+
+    # If day is non-weekend, lower workdays_to_substract by one
+    while workdays_to_substract > 0:
+        temp_date -= timedelta(days=1)
+        if temp_date.weekday() < 5:
+            workdays_to_substract -= 1
+        all_days_to_substract += 1
+
+    return all_days_to_substract
