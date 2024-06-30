630cfd0d8729b358eeff763d3a500f755b7d678b
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-06-30 22:08:40 +0100

    v0.06.4 - create file and add description

--- analyzer_src/fibonacci_retracements_hex.py
+++ analyzer_src/fibonacci_retracements_hex.py
@@ -1,3 +1,11 @@
 """
 Fibonacci Retracements hex:
+Theoretical lines that helps assesing critical points on charts such
+as - supports, resistances, target prices, trend reversals etc. They 
+are calculated by taking two important points from the chart (e.g. 52 
+weeks low and high) substracting them and dividing calculated distance 
+by specified percentage ratios: 23.6%, 38.2%, 50%, 61.8%, and 100%. 
+FR must be used with other TA items, as it usually works as the self-
+-fulfilling prophecy. It's worth to consider little bit smaller/bigger
+percentages to get ahead other traders.
 """
