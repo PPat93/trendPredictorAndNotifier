dd69fa86e13a870661ebd81b98b586312b315b75
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-07-04 20:02:18 +0100

    v0.07.0 - further plotter preparations

--- plotter_cmb.py
+++ plotter_cmb.py
@@ -4,4 +4,16 @@ Sends/Receives data from Integrator: <- Analyzer, <- Database, -> Mailer;
 Creates graphs for content of notification emails;
 """
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+def plot_graph(data, ticker, start_date, end_date, *horizontal_lines):
+
+    fig, ax = plt.subplots()
+    ax.plot(data)
+    ax.legend()
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    ax.set_title(f"{ticker} Stock Price ({start_date} to {end_date})")
+    plt.show()
+
+plot_graph([1, 2, 3], "NVDA", "121", "12")
