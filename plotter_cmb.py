766f2a782c0bf2262f2efe2afe33ccd7fb621099
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-07-08 21:16:30 +0100

    v0.07.0 - finish first plotter version

--- plotter_cmb.py
+++ plotter_cmb.py
@@ -15,10 +15,9 @@ def plot_one_graph(data, graph_name, ticker, start_date, end_date, *horizontal_l
     ax.set_xlabel("Date")
     ax.set_ylabel(graph_name)
     ax.set_title(f"{ticker} Stock {graph_name} graph ({start_date} to {end_date})")
-    ax.hlines(
-        y=horizontal_lines, xmin=start_date, xmax=end_date, linewidth=2, color="r"
-    )
+    for line in horizontal_lines:
+        ax.axhline(y=line, c="green", linewidth=0.5, zorder=0, linestyle="--")
     plt.show()
 
 
-# plot_one_graph([1, 2, 3], "MACD", "NVDA", "1", "12", 4)
+# plot_one_graph([1, 2, 3], "MACD", "NVDA", "1", "12", 4, 5, 6)
