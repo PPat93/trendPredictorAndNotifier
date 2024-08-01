eabde1d1078b7325fc0aff82c4ad46f18dd07bd9
PPat93 <45639693+PPat93@users.noreply.github.com>
2024-08-01 23:01:38 +0100

    v0.10.0 - add automatic div retriever

--- utils/div_download.py
+++ utils/div_download.py
@@ -6,19 +6,16 @@ from dateutil.relativedelta import relativedelta
 def get_div(ticker):
     today = dt.date.today()
     last_year = dt.date.today() - relativedelta(years=3, days=1)
-    divs = yf.download(ticker, last_year, today, actions=True, interval="1mo")
-    print(divs["Dividends"])
+    res = yf.download(ticker, last_year, today, actions=True, interval="1mo")
+    divs = res["Dividends"]
+    print(ticker + "  " + str(divs.iloc[0]))
 
 
-# def replace
-# with open(
-#     "D:\\projekty_py\\trend_predictor\\utils\\paths.csv", encoding="utf-8"
-# ) as input_file, open("outfile.csv", "w", encoding="utf-8") as output_file:
+with open(
+    "D:\\projekty_py\\trend_predictor\\utils\\tickerTest.csv", encoding="utf-8"
+) as input_file:
 
-#     for line in input_file:
-
-#         line = str(line).replace("C:\\Users\\Piotrek\\Downloads\\wse stocks\\", "")
-#         line = str(line).replace(".txt", "")
-#         output_file.write(line)
-
-get_div("vrg.wa")
+    for line in input_file:
+        full_ticker = f"{line.strip()}.wa"
+        print(full_ticker)
+        get_div(full_ticker)
